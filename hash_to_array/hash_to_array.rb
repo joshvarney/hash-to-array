1b46a1a3e0b7b9be8361427aa99dcd311ac7baeb
Josh Varney <joshcainvarney@gmail.com>
2018-07-02 12:38:26 -0400

    Add files via upload

--- hash_to_array/hash_to_array.rb
+++ hash_to_array/hash_to_array.rb
@@ -2,12 +2,10 @@
 def my_hash_to_array(my_hash)
 final_arr = []
 my_hash.each do |key, value| 
-key
-value
 new_arr = []
 new_arr << key
 new_arr << value
 final_arr << new_arr
 end
-p final_arr 
+final_arr 
 end
