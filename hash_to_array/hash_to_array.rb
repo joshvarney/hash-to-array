bb4d6c1267e9c288299ccd0a4a83d95b1331b653
Josh Varney <joshcainvarney@gmail.com>
2018-07-06 00:31:35 -0400

    Add files via upload

--- hash_to_array/hash_to_array.rb
+++ hash_to_array/hash_to_array.rb
@@ -1,11 +1,10 @@
-
 def my_hash_to_array(my_hash)
-final_arr = []
-my_hash.each do |key, value| 
-new_arr = []
-new_arr << key
-new_arr << value
-final_arr << new_arr
-end
-final_arr 
+	final_arr = []
+	my_hash.each do |key, value| 
+		new_arr = []
+		new_arr << key
+		new_arr << value
+		final_arr << new_arr
+	end
+	final_arr 
 end
--- hash_to_array/hash_to_array_test.rb
+++ hash_to_array/hash_to_array_test.rb
@@ -4,11 +4,10 @@ class Test_Hash_to_array < Minitest::Test
 
 	def test_assert_that_hash_to_array_works
 		my_hash = {
-	"batman" => "bruce wayne",
-	"superman" => "clark kent",
-	"flash" => "barry allen"
-}
+			"batman" => "bruce wayne",
+			"superman" => "clark kent",
+			"flash" => "barry allen"
+		}
 		assert_equal([["batman", "bruce wayne"], ["superman", "clark kent"], ["flash", "barry allen"]], my_hash_to_array(my_hash))
 	end
-	
 end
